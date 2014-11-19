c28b506fc035aee175671806f36baaac25659dcc
anthony.yarosh <anthony.yarosh@MU-C3-05.mullen.pvt.k12.co.us>
2014-11-19 10:44:17 -0700

    2nd commit

--- Inheritance of Animal/src/Penguin.java
+++ Inheritance of Animal/src/Penguin.java
@@ -3,19 +3,19 @@ public class Penguin extends Animal
 {
 public void bearsYoung()
 	{
-	
+	System.out.println("Ooo, a cute little egg!");
 	
 	}
 
 public void makesNoise()
 	{
-	
+	System.out.println("The penguin says, \"Brrr...\"");
 	
 	}
 
 public void eats()
 	{
-	
+	System.out.println("The penguin is looking forward to eating some fish.");
 	
 	}
 
